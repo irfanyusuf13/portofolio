b78b23a246de7c935b8aca644981a419d9e0f53b
irfanyusuf13 <irfan.yusuf@ui.ac.id>
2025-06-13 18:12:24 +0700

    fix project section for mobile (semoga)

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -15,7 +15,7 @@ export const projectsData: IProject[] = [
     title: 'Environment Monitoring System',
     description: 'Environmental monitoring system using ESP32 to measure and monitor air quality index and temperature in real time,',
     tags: ['C++', 'ESP32'],
-    image: '/images/projects/EMS.png', 
+    image: '/images/projects/EMS.webp', 
     githubUrl: 'https://github.com/irfanyusuf13/Enviromental-Monitoring-System',
     role: 'Coder and Hardware Designer', 
     date: 'December 2024'
@@ -24,7 +24,7 @@ export const projectsData: IProject[] = [
     title: 'BikeGuard',
     description: 'Website Based Bicycle Security System with lock and qr code',
     tags: ['React', 'Tailwind CSS', 'Node.js', 'Express.js', 'PostgreSQL', 'C++', 'ESP32'],
-    image: '/images/projects/BikeGuard.png', 
+    image: '/images/projects/BikeGuard.webp', 
     githubUrl: 'https://github.com/irfanyusuf13/BikeGuardAsli',
     role: 'IoT Developer, Frontend Developer, Project Management', 
     date: 'December 2024'
@@ -33,7 +33,7 @@ export const projectsData: IProject[] = [
     title: 'LibCatalog',
     description: 'Create a full-stack library management system website using SERN stack.',
     tags: ['React', 'Tailwind CSS', 'Node.js', 'Express', 'PostgreSQL'],
-    image: '/images/projects/LibCatalog.png',
+    image: '/images/projects/LibCatalog.webp',
     githubUrl: 'https://github.com/SistemBasisData2024/LibCatalog',
     role: 'Backend Developer',
     date: 'June 2024'
@@ -42,7 +42,7 @@ export const projectsData: IProject[] = [
     title: 'Smart Chiller',
     description: 'A smart temperature control system for food storage',
     tags: ['Assembly', 'Proteus'],
-    image: '/images/projects/SmartChiller.png',
+    image: '/images/projects/SmartChiller.webp',
     githubUrl: 'https://github.com/irfanyusuf13/Smart-Chiller-Kel19-',
     role: 'Coder and Hardware Designer',
     date: 'May 2024'
@@ -51,7 +51,7 @@ export const projectsData: IProject[] = [
     title: 'JBus',
     description: 'Create an Android application for bus ticket booking and management using Java',
     tags: ['Java', 'Android Studio'],
-    image: '/images/projects/JBus.png', 
+    image: '/images/projects/JBus.webp', 
     githubUrl: 'https://github.com/irfanyusuf13/JBus-android',
     role: 'Full-Stack Developer', 
     date: 'December 2023'
@@ -60,7 +60,7 @@ export const projectsData: IProject[] = [
     title: 'Automated Plant Watering System',
     description: 'Automated Plant Watering System using VHDL based on moisutre and schedule',
     tags: ['VHDL', 'ModelSim'],
-    image: '/images/projects/PSD.png', 
+    image: '/images/projects/PSD.webp', 
     githubUrl: 'https://github.com/irfanyusuf13/Finpro-PSD---AP02/tree/master', 
     date: 'December 2023'
   },
@@ -68,14 +68,14 @@ export const projectsData: IProject[] = [
     title: 'Network Design and Simulation',
     description: 'Design and simulate large scale networks using Cisco Packet Tracer',
     tags: ['Cisco Packet Tracer'],
-    image: '/images/projects/Jarkom.png', 
+    image: '/images/projects/Jarkom.webp', 
     date: 'December 2023'
   },
   {
     title: 'Smart Traffic Light',
     description: 'Creating a Smart Traffic Light System using the Proteus application',
     tags: ['Proteus'],
-    image: '/images/projects/DSD.png', 
+    image: '/images/projects/DSD.webp', 
     date: 'December 2022'
   }
 ];
