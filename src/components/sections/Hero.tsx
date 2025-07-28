8a640105fdb33b20c89cf2eb9f8f34fabf3acfaa
irfanyusuf13 <irfan.yusuf@ui.ac.id>
2025-07-28 11:35:53 +0700

    hapus sesuatu

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
   return (
@@ -8,20 +7,6 @@ const Hero = () => {
         <h1 className="text-4xl md:text-6xl lg:text-7xl">
           Irfan Yusuf Khaerullah
         </h1>
-        <TypeAnimation
-          sequence={[
-            'AI/ML', 2000,
-            'Data Analytic', 2000,
-            'Software Engineer', 2000,
-            'Computer Networks', 2000,
-            'IoT System', 2000,
-
-          ]}
-          wrapper="span"
-          speed={50}
-          className="text-2xl md:text-4xl text-cyan-400 mt-3 block"
-          repeat={Infinity}
-        />
         <p className="max-w-3xl mx-auto mt-6 text-lg text-gray-300">
           Computer Engineering student with a strong interest in AI/ML, data-driven systems, software development, computer networks, and IoT
         </p>
